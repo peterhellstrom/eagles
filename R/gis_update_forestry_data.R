b5d32ea22b1ba433750e2b0da7ee4cf62dae5014
peterhellstrom <peter.hellstrom@nrm.se>
2023-08-23 09:09:26 +0200

    added sks_last_updated()

--- R/gis_update_forestry_data.R
+++ R/gis_update_forestry_data.R
@@ -1,4 +1,38 @@
 #' @export
+sks_last_updated <- function(
+    feed_name,
+    base_url = "https://geodpags.skogsstyrelsen.se/geodataport",
+    datetime_format = "%Y-%m-%dT%H:%M:%SZ",
+    string_format = "%Y%m%d_%H%M%S") {
+
+  feed_url <- glue::glue("{base_url}/feeds/{feed_name}.xml")
+  # download_url <- glue::glue("{base_url}/data/{file_name}.zip")
+
+  last_updated <- xml2::read_xml(feed_url) |>
+    xml2::xml_ns_strip() |>
+    xml2::xml_find_all("updated") |>
+    xml2::xml_text() |>
+    readr::parse_datetime(datetime_format) |>
+    base::format(string_format)
+
+  last_updated
+}
+
+# Only download file if it does NOT exist in download directory
+# Potential problem - if previous download was incomplete/interrupted?
+#' @export
+download_cmd <- function(download_url, download_file) {
+  if (!file.exists(download_file)) {
+    system(
+      command = "cmd.exe",
+      input = glue::glue("curl -o {download_file} {download_url}"),
+      show.output.on.console = TRUE)
+  } else {
+    cat(glue::glue("Data file {download_file} already exists!"), "\n")
+  }
+}
+
+#' @export
 list_files_compare <- function(
     base_dir = "./downloads",
     prefix = "sksAvverkAnm",
